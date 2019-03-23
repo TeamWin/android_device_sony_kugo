95fe5726da591fc3aac0c8e39d6fae664386706d
dianlujitao <dianlujitao@lineageos.org>
2019-03-23 16:17:36 +0800

    kugo: Fix HW FDE when building with Pie tree
     * ro.board.platform is no longer part of recovery ramdisk on Pie, thus
       hw_get_module_by_class would fail to find the keystore module and HW
       FDE is broken.
     * To address the issue, use a property to specify the HAL class to make
       the HAL picked by libhardware.
    
    Change-Id: I2b09a06be34ca66a400e98fce8714226ae5f3d10

--- device.mk
+++ device.mk
@@ -22,6 +22,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.usb.pid_suffix=1E8
 
+# HW Keystore
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=msm8952
+
 # Time Zone data for recovery
 PRODUCT_COPY_FILES += \
     system/timezone/output_data/iana/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
